0a5a324a58c5307cc0bac557e3136b7ce58d5e32
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-10-18 10:28:58 -0600

    Added a bonus problem but did not finish

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ function makeUnique (str) {
 
 
  console.log(makeUnique('iwanttoclimbamountain'))
+
+
+ //Bonus problem
+
+//  Given a rectangular, two-dimensional array filled with either value 1 or value 0,
+// write a function that returns the same array with any positions "adjacent" to a 1
+// also turned into 1s.
+
+// Example:
+// Function input:
+// [
+//     [0,1,0,0,0,1],
+//     [0,0,0,0,0,0],
+//     [0,0,0,1,0,0],
+//     [0,0,0,0,0,0]
+// ]
+// Function output:
+// [
+//     [1,1,1,0,1,1],
+//     [0,1,0,1,0,1],
+//     [0,0,1,1,1,0],
+//     [0,0,0,1,0,0]
+// ]
